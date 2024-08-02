de74309f115ee9329401d7e9d39d63ea5c79e4e3
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-08-02 21:31:48 +0530

    contact details added

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Services from './Components/Services/Services'
 import MyWork from './Components/MyWork/MyWork'
+import Contact from './Components/Contact/Contact'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <About />
-      <Services/>
-      <MyWork/>
+      <Services />
+      <MyWork />
+      <Contact />
     </div>
   )
 }
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import mail_icon from '../../assets/mail_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+
+const Contact = () => {
+    return (
+        <div className='contact'>
+            <div className="contact-title">
+                <h1>Get in touch</h1>
+                <img src={theme_pattern} alt="" />
+            </div>
+            <div className="contact-section">
+                <div className="contact-left">
+                    <h1>Let's talk</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur, impedit?</p>
+                    <div className="contact-details">
+                        <div className="contact-detail">
+                            <img src={mail_icon} alt="" />
+                            <p>rahulcoder00@gmail.com</p>
+                        </div>
+                        <div className="contact-detail">
+                            <img src={call_icon} alt="" />
+                            <p>+91 99887-76655</p>
+                        </div>
+                        <div className="contact-detail">
+                            <img src={location_icon} alt="" />
+                            <p>Greater Noida, 201310</p>
+                        </div>
+                    </div>
+                </div>
+                <form className='contact-right'>
+                    <label htmlFor="">Your Name</label>
+                    <input type="text" placeholder='Enter your name' name='name' />
+
+                    <label htmlFor="">Your Email</label>
+                    <input type="email" placeholder='Enter your email' name='email' />
+
+                    <label htmlFor="">Write your message here</label>
+                    <textarea name="message" rows="8" placeholder='Enter your message'></textarea>
+                    <div className="contact-submit">Submit now</div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
