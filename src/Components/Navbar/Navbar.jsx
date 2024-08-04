f22e6b64598d09de3a5aa3bc5d08e5f1b47b7da1
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-08-05 01:33:35 +0530

    Responsive using @media CSS max-width: 480px;

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react'
 import './Navbar.css'
-import logo from '../../assets/logo.svg'
 import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import menu_open from '../../assets/menu_open.svg'
@@ -20,7 +19,7 @@ const Navbar = () => {
 
     return (
         <div className='navbar'>
-            <img src={logo} alt="" />
+            <div className='logo'>PortFolio</div>
             <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open' />
             <ul ref={menuRef} className="nav-menu">
                 <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />
