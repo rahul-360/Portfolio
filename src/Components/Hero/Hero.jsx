ace97beecd51b67f350ffd63ba495e0cfdcb1481
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-08-04 22:17:18 +0530

    update profile_img

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Hero.css"
-import profile_img from '../../assets/profile_img.svg'
+import profile_img from '../../assets/profile_img.jpg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
